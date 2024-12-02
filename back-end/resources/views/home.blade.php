6b09203ed26b9a464987033122810c16a3aab15f
Gehad Gallo <macbook@Gehad-Gallo.home>
2024-12-02 10:51:25 +0200

    Mail verification after registeration and some edit in breeze with adminLTE

--- back-end/resources/views/home.blade.php
+++ back-end/resources/views/home.blade.php
@@ -6,14 +6,14 @@
     <h1>Dashboard</h1>
 @stop
 
-
 @section('content')
     <p>Welcome to this beautiful admin panel.</p>
 @stop
 
 @section('css')
-    {{-- Add here extra stylesheets --}}
+    {{-- Add extra stylesheets here --}}
     {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+    <link rel="icon" type="image/png" href="{{ asset('/images/favicon.png') }}"> 
 @stop
 
 @section('js')
