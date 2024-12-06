bd3f2dad52e87ec8021a671b6af76f4f020d7bb4
Gehad Gallo <macbook@Gehad-Gallo.home>
2024-12-06 12:33:30 +0200

    Category CRUD operations

--- back-end/resources/views/dashboard/categories/index.blade.php
+++ back-end/resources/views/dashboard/categories/index.blade.php
@@ -3,35 +3,74 @@
 @section('title', 'Categories')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>Categories</h1>
     <br>
 @stop
 
 @section('content')
 
-<div class="d-flex justify-content-end pe-3 mb-3">
-    <button type="button" onclick="window.location='{{ route('categories.create') }}'"
-    class="btn btn-success btn-sm">Create Category</button>
-</div>
-@if($categories->count() > 0)
-    <div class="row">
-        @foreach ($categories as $category)
-        <div class="col-sm-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title font-weight-bold">{{ $category->name }}</h5><br>
-                    <hr>
-                    <p class="card-text">Slug: {{ $category->slug }}</p>
-                    <p class="card-text text-secondary">{{ $category->description }}</p>
-                    <a href="#" class="btn btn-primary ">Edit</a>
-                </div>
-            </div>
+<div class="container-fluid">
+    @if(session()->has('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
         </div>
-        @endforeach
+    @endif
+    <div class="justify-content-center">
+        <div class="col-md-12">
+            <div class="d-flex justify-content-end pe-3 mb-3">
+                <button type="button" onclick="window.location='{{ route('categories.create') }}'" class="btn btn-success btn-sm">Create Category</button>
+            </div>
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col" style="width: 20%;">Slug</th>
+                        <th scope="col" style="width: 30%;">Description</th>
+                        <th scope="col">Parent</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Image</th>
+                        <th scope="col">Operations</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @if($categories->count() > 0)
+                        @foreach ($categories as $category)
+                        <tr>
+                            <th scope="row">{{ $category->name }}</th>
+                            <td>{{ $category->slug }}</td>
+                            <td>{{ $category->description }}</td>
+                            <td>{{ $category->parent_id ? $category->parent_id : 'No Parent' }}</td>
+                            <td>{{ $category->status }}</td>
+                            <td>
+                                @if($category->image)
+                                    <img src="{{ asset('path_to_images/'.$category->image) }}" alt="{{ $category->name }}" width="50">
+                                @else
+                                    No Image
+                                @endif
+                            </td>
+                            <td>
+                                <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-success d-inline-block me-2">Edit</a>
+
+                                <form action="{{ route('categories.destroy', $category->id) }}" method="POST" style="display: inline;" onsubmit="return confirmDelete()">
+                                    @csrf
+                                    @method('DELETE')
+                                    
+                                    <button type="submit" class="btn btn-danger d-inline-block">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    @else
+                        <tr>
+                            <td colspan="7" class="text-center">No Categories</td>
+                        </tr>
+                    @endif
+                </tbody>
+            </table>
+        </div>
     </div>
-@else
-    <p>No Categories</p>
-@endif
+</div>
+
 
 @stop
 
@@ -45,5 +84,9 @@
 @stop
 
 @section('js')
-    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+    <script>
+        function confirmDelete() {
+            return confirm('Are you sure you want to delete this category?');
+        }
+    </script>
 @stop
