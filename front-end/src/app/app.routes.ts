61150254f2b2774c4708935a9f35714b61466d9e
ahmedhussein70 <ahmed.hussein201170@gmail.com>
2024-12-06 14:52:41 +0200

    make page not found , login and register

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './Pages/sign-in/sign-in.component';
+import { SignUpComponent } from './Pages/sign-up/sign-up.component';
+import { PageNotFoundComponent } from './Pages/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: SignInComponent },
+  { path: 'login', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
